79b1af0117ad75c1b9e3d37f8d8751d1c5ab6deb
Leonardo Morini <leo.morini.nn@gmail.com>
2018-09-27 10:06:05 -0300

    Update App.js

--- App.js
+++ App.js
@@ -9,13 +9,16 @@ _`| / __`| / __`| / __`| / .-^  ___
 |._/   |._/   |._/   |._/       ^^^   
  */
 
+//Importações
 import React, { Component } from 'react';
 
+//Importação do componente personalizado que criamos
 import Chatbot from './src/components/chatbot';
 
 export default class App extends Component {
   render() {
     return (
+      //Exportação do componente
       <Chatbot />
     );
   }
